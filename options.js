82d62055e3a5028d17fd99067380a4c6cbd137fb
Charlie Helmich <chelmi98@gmail.com>
2013-12-18 19:51:24 -0700

    Tidied up options page

--- options.js
+++ options.js
@@ -1,14 +1,23 @@
-function save_options() {
+function add_option() {
 	var oldwrd = document.getElementById("oldwrd");
 	var newwrd = document.getElementById("newwrd");
+	
 	if(oldwrd.value != "" && newwrd.value != "") {
 		localStorage[oldwrd.value] = newwrd.value;
+
+		oldwrd.value = "";
+		newwrd.value = "";
+		restore_options();
+	}
+}
+
+function remove_option() {
+	var btn = document.getElementById("del");
+	
+	if (btn.value != "") {
+		localStorage.removeItem(btn.value)
 		
-		var status = document.getElementById("status");
-		status.innerHTML = "Options Saved.";
-		setTimeout(function() {
-			status.innerHTML = "";
-		}, 750);
+		btn.value = "";
 		restore_options();
 	}
 }
@@ -16,20 +25,12 @@ function save_options() {
 function restore_options() {
 	var existingWrds = "";
 	for(i in localStorage) {
-		existingWrds += i + " - " + localStorage[i] + "<br>";
+		existingWrds += "'" + i + "' - '" + localStorage[i] + "'<br>";
 	}
 	var wrdList = document.getElementById("storage");
 	wrdList.innerHTML = existingWrds;
 }
 
-function remove_option() {
-	var btn = document.getElementById("del");
-	if (btn.value != "") {
-		localStorage.removeItem(btn.value)
-		restore_options();
-	}
-}
-
 document.addEventListener('DOMContentLoaded', restore_options);
-document.querySelector('#add').addEventListener('click', save_options);
+document.querySelector('#add').addEventListener('click', add_option);
 document.querySelector('#delGo').addEventListener('click', remove_option);
